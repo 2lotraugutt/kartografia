e9ab8d35d8f086563a731a14dfa41a5ef941bc1f
franek <mgrkot@proton.me>
2025-03-10 18:37:45 +0100

    .

--- update_mac.sh
+++ update_mac.sh
@@ -1,8 +1,15 @@
+[ -z "$1" ] && echo Usage: update_mac.sh arp-scan.txt ipaddr.tsv && exit
+[ -z "$2" ] && echo Usage: update_mac.sh arp-scan.txt ipaddr.tsv && exit
+
+cp $2 $2.back
 IFS=$'\n'
-for i in $(grep -E '([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}' ipaddr.tsv)
+for i in $(grep -E '([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}' $2)
 do
-  ip=$(echo $i|awk '{print $1}')
-  newmac=10:00:00:00:00:01
+  rip=$(echo "$i"|awk -v FS='\t' '{print $1}')
+  ip=$(echo "$i"|awk -v FS='\t' '{print $1}'|sed 's/\./\\\./g' | sed 's/.*/\\b\0\\b/')
+  [ -z $ip ] && continue
+  newmac=$(cat $1 | grep "$ip" | awk '{print $2}' | tr -d ' \t')
+  [ -z "$newmac" ] && echo "[-] Not found mac for $rip" && continue
   newline=$(echo $i | sed -E "s/([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}/$newmac/")
-  sed -i "s/$i/$newline/" ipaddr.tsv
+  sed -i "s/$i/$newline/" $2
 done
